a1f718c73d0458480ec6b659a1108dfa857edf2a
ATMM Tech <aghead@atmmtech.com>
2026-08-20 15:30:15 +0200

    Optimize Outfit font loading for mobile performance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import { Outfit } from "next/font/google";
 import { ThemeProvider } from "@/components/ui/ThemeContext";
 import { PreloadProvider } from "@/hooks/usePreloadContext";
 import { Main } from "@/components/layout/Main";
@@ -6,17 +7,24 @@ import { Preloader } from "@/components/ui/Preloader";
 import "@/styles/globals.css";
 import "@/styles/animations.css";
 
+const outfit = Outfit({
+  subsets: ["latin"],
+  variable: "--font-outfit",
+  display: "swap",
+});
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <PreloadProvider>
-        <div className="font-sans antialiased selection:bg-primary/20 selection:text-primary">
-          <Preloader />
-          <Main>
+    <div className={outfit.variable}>
+      <ThemeProvider>
+        <PreloadProvider>
+          <div className="font-sans antialiased selection:bg-primary/20 selection:text-primary">
+            <Preloader />
+            <Main>
             <Component {...pageProps} />
           </Main>
         </div>
       </PreloadProvider>
     </ThemeProvider>
+    </div>
   );
 }
