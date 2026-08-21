8865b0809ce81d3e02610b3dc5373a35d3855a64
ATMM Tech <aghead@atmmtech.com>
2026-08-21 19:08:50 +0200

    Add responsive Next image sizes

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,8 @@ const nextConfig = {
   reactStrictMode: true,
 
   images: {
+    deviceSizes: [320, 384, 640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 280, 384],
     remotePatterns: [
       {
         protocol: "https",
