723d0e69e3fc11dbfba33b3abac541b8e2226f25
ATMM Tech <aghead@atmmtech.com>
2026-08-21 16:26:08 +0200

    Optimize About image responsive size

--- components/sections/AboutPreviewSection.tsx
+++ components/sections/AboutPreviewSection.tsx
@@ -83,7 +83,7 @@ export function AboutPreviewSection({
                   alt="Aghead - Full-Stack Developer"
                   width={600}
                   height={700}
-                  sizes="(max-width: 640px) 90vw, (max-width: 1024px) 45vw, 500px"
+                  sizes="(max-width: 640px) 420px, (max-width: 1024px) 45vw, 500px"
                   className="w-full h-auto object-cover"
                 />
               </div>
